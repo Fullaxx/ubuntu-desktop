dac9e4460705abf3f7328e775c5e1071f443dbb4
Brett Kuskie <fullaxx@gmail.com>
2019-02-28 07:05:21 -0700

    switched from tail to foreground flag to keep the docker open

--- app.sh
+++ app.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 VNCDIR="/root/.vnc"
+VNCRES="-geometry 1280x800"
+VNCDEPTH="-depth 24"
 
 chmod 0600 ${VNCDIR}/passwd
 
-tigervncserver -geometry 1280x800 -depth 24 -xstartup ${VNCDIR}/xstartup && \
-tail -F /root/.vnc/*.log
+tigervncserver -fg ${VNCRES} ${VNCDEPTH} -xstartup ${VNCDIR}/xstartup
 
 # tightvncserver
 # USER="root" HOME="/root" vncserver :1 -geometry 1280x800 -depth 24 && tail -F /root/.vnc/*.log
