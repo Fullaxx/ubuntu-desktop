16226af1748ccbe566dc5e6161cae0086a074279
Brett Kuskie <fullaxx@gmail.com>
2019-03-01 09:32:09 -0700

    Added VNC configuration via ENV variables

--- tiger.sh
+++ tiger.sh
@@ -2,10 +2,21 @@
 
 VNCDIR="/root/.vnc"
 VNCAUTH="-SecurityTypes=None --I-KNOW-THIS-IS-INSECURE"
-VNCRES="-geometry 1280x800"
-VNCDEPTH="-depth 24"
-VNCSTART="-xstartup ${VNCDIR}/xstartup"
+VNCRES=${VNCRES:-1280x800}
+VNCDEPTH=${VNCDEPTH:-24}
+VNCSTART="${VNCDIR}/xstartup"
 
-# chmod 0600 ${VNCDIR}/passwd
+# If a password was specified on the command line
+if [ -n "${VNCPASS}" ]; then
+  echo "${VNCPASS}" | vncpasswd -f > ${VNCDIR}/passwd
+  if [ -n "${VNCPASSRO}" ]; then
+    echo -e "${VNCPASS}\n${VNCPASS}\ny\n${VNCPASSRO}\n${VNCPASSRO}\n" | vncpasswd
+  fi
+  chmod 0600 ${VNCDIR}/passwd
+  VNCAUTH=""
+fi
 
-tigervncserver -fg -localhost no ${VNCAUTH} ${VNCRES} ${VNCDEPTH} ${VNCSTART}
+tigervncserver -fg -localhost no ${VNCAUTH} \
+-geometry ${VNCRES} \
+-depth ${VNCDEPTH} \
+-xstartup ${VNCSTART}
