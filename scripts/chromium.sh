555e17eae6e3bae6795eeacf2453c866965b1a3b
Brett Kuskie <fullaxx@gmail.com>
2019-03-03 06:09:36 -0700

    Added comments to scripts/chromium.sh

--- scripts/chromium.sh
+++ scripts/chromium.sh
@@ -11,4 +11,6 @@ if [ "$?" -ne "0" ]; then
   xterm -T AptGetInstall -g 100x30 -e apt-get install -y ${BINARY}
 fi
 
+# https://www.ghacks.net/2013/10/06/list-useful-google-chrome-command-line-switches/
+# https://peter.sh/experiments/chromium-command-line-switches/
 ${BINARY} --no-sandbox --disable-gpu --disable-accelerated-2d-canvas --disable-gpu-rasterization
