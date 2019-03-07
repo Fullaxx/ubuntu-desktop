2db08ee619c9ac584ade9219dcd4fc404f27bb61
Brett Kuskie <fullaxx@gmail.com>
2019-03-07 05:19:51 -0700

    added build-essential to bluefish and geany scripts

--- scripts/geany.sh
+++ scripts/geany.sh
@@ -8,7 +8,7 @@ if [ "$?" -ne "0" ]; then
   set -e
 
   xterm -T AptGetUpdate -g 100x30 -e apt-get update
-  xterm -T AptGetInstall -g 100x30 -e apt-get install -y ${BINARY}
+  xterm -T AptGetInstall -g 100x30 -e apt-get install -y build-essential ${BINARY}
 fi
 
 ${BINARY}
