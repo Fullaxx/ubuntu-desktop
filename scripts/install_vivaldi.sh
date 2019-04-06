aaa23dcc938769f490f13078e96adead0de127bc
Brett Kuskie <fullaxx@gmail.com>
2019-04-06 09:09:33 -0600

    Fix scripts/install_vivaldi.sh so as not to break future apt-get update commands

--- scripts/install_vivaldi.sh
+++ scripts/install_vivaldi.sh
@@ -7,8 +7,9 @@ URL="https://downloads.vivaldi.com/stable/vivaldi-stable_2.4.1488.36-1_amd64.deb
 DEB="/tmp/vivaldi.deb"
 
 apt-get update
-apt-get install -y chromium-codecs-ffmpeg-extra
-# apt-get install -y gnupg2 software-properties-common apt-transport-https wget chromium-codecs-ffmpeg-extra dpkg-sig
+# apt-get install -y chromium-codecs-ffmpeg-extra
+apt-get install -y gnupg2 software-properties-common apt-transport-https wget chromium-codecs-ffmpeg-extra dpkg-sig
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8D04CE49EFB20B23
 
 # wget -q https://repo.vivaldi.com/stable/linux_signing_key.pub -O- | apt-key add -
 # add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
