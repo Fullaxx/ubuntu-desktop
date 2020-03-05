354ef58e299942c26451f66e31185ef7045b1fd0
Brett Kuskie <fullaxx@gmail.com>
2020-03-05 13:35:04 -0700

    Update install_vscode.sh with version-defined deb file

---
+++ scripts/install_vscode.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+export DEBIAN_FRONTEND="noninteractive"
+URL="https://update.code.visualstudio.com/1.42.1/linux-deb-x64/stable"
+DEB="/tmp/vscode.deb"
+
+wget ${URL} -O ${DEB}
+dpkg -i ${DEB} || (set -e; apt-get update; apt-get install -f -y; apt-get install -y libasound2)
+rm ${DEB}
