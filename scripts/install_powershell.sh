9bb52186e49a1af7a6dbae09517955d9098eac00
Brett Kuskie <fullaxx@gmail.com>
2020-08-21 15:51:57 -0600

    Update Powershell to 7.0.3

--- scripts/install_powershell.sh
+++ scripts/install_powershell.sh
@@ -3,8 +3,10 @@
 set -e
 
 export DEBIAN_FRONTEND="noninteractive"
-URL="https://github.com/PowerShell/PowerShell/releases/download/v6.2.0/powershell_6.2.0-1.ubuntu.18.04_amd64.deb"
-SHA="38DCB2164C1FE3B4F0200ABA2D5969C4856526BAA1D8983F818CD06003C355CE"
+#URL="https://github.com/PowerShell/PowerShell/releases/download/v6.2.0/powershell_6.2.0-1.ubuntu.18.04_amd64.deb"
+#SHA="38DCB2164C1FE3B4F0200ABA2D5969C4856526BAA1D8983F818CD06003C355CE"
+URL="https://github.com/PowerShell/PowerShell/releases/download/v7.0.3/powershell-lts_7.0.3-1.ubuntu.18.04_amd64.deb"
+SHA="F62E5E6FCF9E0590FB867F0A2231EDD76F54B8135D8220A632413D5BA7942733"
 DEB="/tmp/powershell.deb"
 
 wget ${URL} -O ${DEB}
