e9fe637d8471d09b388d332e88d82842a591e4cb
Brett Kuskie <fullaxx@gmail.com>
2020-08-21 16:12:18 -0600

    Update to Tor-9.5.3

--- scripts/install_torbrowser_manual.sh
+++ scripts/install_torbrowser_manual.sh
@@ -11,17 +11,14 @@ apt-get update
 apt-get install -y file libgtk-3-0 libdbus-glib-1-2 wget xz-utils
 
 # Download package
-wget https://www.torproject.org/dist/torbrowser/9.0.4/tor-browser-linux64-9.0.4_en-US.tar.xz -O /tmp/tor.tar.xz
+SHA="648931503b886d687828df9730aa284b7c18303682cc82f06c9da4a734c81bcf"
+wget https://www.torproject.org/dist/torbrowser/9.5.3/tor-browser-linux64-9.5.3_en-US.tar.xz -O /tmp/tor.tar.xz
 # wget https://www.torproject.org/dist/torbrowser/9.0.4/tor-browser-linux64-9.0.4_en-US.tar.xz.asc -O /tmp/tor.asc
 # gpg --keyserver pool.sks-keyservers.net --recv-keys D1483FA6C3C07136
 # gpg --verify /tmp/tor.asc /tmp/tor.tar.xz
 
-# GHETTO HACK since key verification is borked
-TORSUM=`sha256sum /tmp/tor.tar.xz | awk '{print $1}'`
-if [ "${TORSUM}" != "53c85059445eff023e7e5ed4749dfdf12b328353e82a4c7263f010120570f493" ]; then
-  echo "PACKAGE CHECKSUM FAIL!"
-  exit 1
-fi
+# Manual verification since key verification is borked
+echo ${SHA} /tmp/tor.tar.xz | sha256sum -c
 
 # Install package
 tar xf /tmp/tor.tar.xz -C /opt
