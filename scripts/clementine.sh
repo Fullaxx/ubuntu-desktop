acb31033c7476b2a069412301fc8a9b72c528384
Brett Kuskie <fullaxx@gmail.com>
2020-09-10 10:12:42 -0600

    Update scripts/clementine.sh to only install libssl-1.0 on bionic

--- scripts/clementine.sh
+++ scripts/clementine.sh
@@ -8,7 +8,12 @@ if [ "$?" -ne "0" ]; then
   set -e
 
   xterm -T AptGetUpdate -g 100x30 -e sudo apt-get update
-  xterm -T AptGetInstall -g 100x30 -e sudo apt-get install -y libssl1.0.0 ${BINARY}
+  xterm -T AptGetInstall -g 100x30 -e sudo apt-get install -y ${BINARY}
+
+  OSV=`grep VERSION_ID /etc/os-release | cut -d\" -f2`
+  if [ "${OSV}" == "18.04" ]; then
+    xterm -T AptGetInstall -g 100x30 -e sudo apt-get install -y libssl1.0.0
+  fi
 fi
 
 ${BINARY}
