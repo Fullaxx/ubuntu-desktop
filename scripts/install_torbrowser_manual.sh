463d0a29c9a13895e428dc6b410d91dc6c2a0325
Brett Kuskie <fullaxx@gmail.com>
2020-09-11 08:04:22 -0600

    Update to tor-9.5.4

--- scripts/install_torbrowser_manual.sh
+++ scripts/install_torbrowser_manual.sh
@@ -11,8 +11,8 @@ apt-get update
 apt-get install -y file libgtk-3-0 libdbus-glib-1-2 wget xz-utils
 
 # Download package
-SHA="648931503b886d687828df9730aa284b7c18303682cc82f06c9da4a734c81bcf"
-wget https://www.torproject.org/dist/torbrowser/9.5.3/tor-browser-linux64-9.5.3_en-US.tar.xz -O /tmp/tor.tar.xz
+SHA="5d6d81db04e0a8c536c3d5e13c935c52318bac7ca44089e031c1bf7c54d66f4e"
+wget https://www.torproject.org/dist/torbrowser/9.5.4/tor-browser-linux64-9.5.4_en-US.tar.xz -O /tmp/tor.tar.xz
 # wget https://www.torproject.org/dist/torbrowser/9.0.4/tor-browser-linux64-9.0.4_en-US.tar.xz.asc -O /tmp/tor.asc
 # gpg --keyserver pool.sks-keyservers.net --recv-keys D1483FA6C3C07136
 # gpg --verify /tmp/tor.asc /tmp/tor.tar.xz
