47efb40fcfa73c4c2cd5dc7cc4453f4872258352
Brett Kuskie <fullaxx@gmail.com>
2020-11-03 06:31:07 -0700

    Update Vivaldi

---
+++ scripts/install_vivaldi_repo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+export DEBIAN_FRONTEND="noninteractive"
+
+apt-get update
+apt-get install -y gnupg2 software-properties-common apt-transport-https wget chromium-codecs-ffmpeg-extra
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys B44B85E3E1A6386BFC79D4119658E8044A3AA3D6
+# wget -q https://repo.vivaldi.com/stable/linux_signing_key.pub -O- | apt-key add -
+
+# add-apt-repository "deb http://repo.vivaldi.com/stable/deb/ stable main"
+echo "deb http://repo.vivaldi.com/stable/deb/ stable main" > /etc/apt/sources.list.d/vivaldi.list
+
+apt-get update
+apt-get install -y vivaldi-stable
--- scripts/vivaldi.sh
+++ scripts/vivaldi.sh
@@ -6,7 +6,7 @@ BINARYLOC=`which ${BINARY}`
 if [ "$?" -ne "0" ]; then
   set -e
 
-  xterm -T InstallVivaldi -g 100x30 -e sudo /app/scripts/install_vivaldi.sh
+  xterm -T InstallVivaldi -g 100x30 -e sudo /app/scripts/install_vivaldi_repo.sh
 fi
 
 if [ `id -u` == "0" ]; then
