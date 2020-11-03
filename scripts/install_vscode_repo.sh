80f564a8a851971e9e8700a7ba9f6fc3c7f592d8
Brett Kuskie <fullaxx@gmail.com>
2020-11-03 05:57:16 -0700

    Fix VSCode install

--- scripts/install_vscode_repo.sh
+++ scripts/install_vscode_repo.sh
@@ -5,11 +5,11 @@ set -e
 export DEBIAN_FRONTEND="noninteractive"
 
 apt-get update
-apt-get install -y gnupg2 software-properties-common apt-transport-https wget libasound2
+apt-get install -y gnupg2 software-properties-common apt-transport-https wget libasound2 libgbm1
 
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | apt-key add -
 # add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/microsoft-vscode.list
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list
 
 apt-get update
 apt-get install -y code
