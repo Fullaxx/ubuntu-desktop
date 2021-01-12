ac50e12f040df2fece0f5a032c8977812ea34ed2
Brett Kuskie <fullaxx@gmail.com>
2021-01-12 08:04:42 -0700

    Update to powershell-7.1.0

--- scripts/install_powershell.sh
+++ scripts/install_powershell.sh
@@ -2,12 +2,24 @@
 
 set -e
 
+bailmsg()
+{
+  xmessage "$1" &
+  exit 1
+}
+
 export DEBIAN_FRONTEND="noninteractive"
-#URL="https://github.com/PowerShell/PowerShell/releases/download/v6.2.0/powershell_6.2.0-1.ubuntu.18.04_amd64.deb"
-#SHA="38DCB2164C1FE3B4F0200ABA2D5969C4856526BAA1D8983F818CD06003C355CE"
-URL="https://github.com/PowerShell/PowerShell/releases/download/v7.0.3/powershell-lts_7.0.3-1.ubuntu.18.04_amd64.deb"
-SHA="F62E5E6FCF9E0590FB867F0A2231EDD76F54B8135D8220A632413D5BA7942733"
+
+OSV=`grep VERSION_ID /etc/os-release | cut -d\" -f2`
+URL="https://github.com/PowerShell/PowerShell/releases/download/v7.1.0/powershell_7.1.0-1.ubuntu.${OSV}_amd64.deb"
 DEB="/tmp/powershell.deb"
+
+case "${OSV}" in
+  '16.04') SHA="25EDE87C86DBB44A3539E9FDFBBF7202359F723C687B9A6DD5951AEDA259C29B";;
+  '18.04') SHA="0E91D6A3EFDE525266E4DC35613415D318408E4DF7C37E1E8576175AB9378FAA";;
+  '20.04') SHA="09AC03BDCD7C74A36807BECA62EB4CCFCA690BE1DC3936ED08A7B8F14FE0CFF9";;
+        *) bailmsg "Ubuntu ${OSV} not supported!"
+esac
 
 wget ${URL} -O ${DEB}
 echo "${SHA} /tmp/powershell.deb" | sha256sum -c
