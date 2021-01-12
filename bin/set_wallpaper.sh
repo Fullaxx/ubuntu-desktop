41dea8b7f572f726ea458302b1839c1ce5b92544
Brett Kuskie <fullaxx@gmail.com>
2021-01-12 07:36:09 -0700

    Update bin/set_wallpaper.sh

--- bin/set_wallpaper.sh
+++ bin/set_wallpaper.sh
@@ -6,13 +6,13 @@ if [ -z "$1" ]; then
 fi
 
 case "$1" in
-  1) WPURL="http://digitalblasphemy.com/graphics/HDfree/acumen1HDfree.jpg" ;;
-  2) WPURL="http://digitalblasphemy.com/graphics/HDfree/foggyxmasevenight1HDfree.jpg" ;;
-  3) WPURL="http://digitalblasphemy.com/graphics/HDfree/pyrexmasHDfree.jpg" ;;
-  4) WPURL="http://digitalblasphemy.com/graphics/HDfree/holidayfluorescence2k141HDfree.jpg" ;;
-  5) WPURL="http://digitalblasphemy.com/graphics/HDfree/automaton1HDfree.jpg" ;;
-  6) WPURL="http://digitalblasphemy.com/graphics/HDfree/portals1HDfree.jpg" ;;
-  7) WPURL="http://digitalblasphemy.com/graphics/HDfree/arcana2HDfree.jpg" ;;
+  1) WPURL="http://digitalblasphemy.com/graphics/HDfree/automaton1HDfree.jpg" ;;
+  2) WPURL="http://digitalblasphemy.com/graphics/HDfree/portals1HDfree.jpg" ;;
+  3) WPURL="http://digitalblasphemy.com/graphics/HDfree/redfern3HDfree.jpg" ;;
+  4) WPURL="http://digitalblasphemy.com/graphics/HDfree/granddesign2HDfree.jpg" ;;
+  5) WPURL="http://digitalblasphemy.com/graphics/HDfree/arcana2HDfree.jpg" ;;
+  6) WPURL="http://digitalblasphemy.com/graphics/HDfree/highlandspring1HDfree.jpg" ;;
+  7) WPURL="http://digitalblasphemy.com/graphics/HDfree/hexaline2HDfree.jpg" ;;
   8) WPURL="http://digitalblasphemy.com/graphics/HDfree/fluorescence6HDfree.jpg" ;;
   *) WPURL="0" ;;
 esac
