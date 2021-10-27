8c0276fff59de06640b60e0babde5ac77a246388
Brett Kuskie <fullaxx@gmail.com>
2021-10-27 15:30:44 -0600

    Update to powershell-7.1.5

--- scripts/install_powershell.sh
+++ scripts/install_powershell.sh
@@ -10,13 +10,13 @@ bailmsg()
 
 export DEBIAN_FRONTEND="noninteractive"
 OSV=`grep VERSION_ID /etc/os-release | cut -d\" -f2`
-URL="https://github.com/PowerShell/PowerShell/releases/download/v7.1.0/powershell_7.1.0-1.ubuntu.${OSV}_amd64.deb"
+URL="https://github.com/PowerShell/PowerShell/releases/download/v7.1.5/powershell_7.1.5-1.ubuntu.${OSV}_amd64.deb"
 DEB="/tmp/powershell.deb"
 
 case "${OSV}" in
-  '16.04') SHA="25EDE87C86DBB44A3539E9FDFBBF7202359F723C687B9A6DD5951AEDA259C29B";;
-  '18.04') SHA="0E91D6A3EFDE525266E4DC35613415D318408E4DF7C37E1E8576175AB9378FAA";;
-  '20.04') SHA="09AC03BDCD7C74A36807BECA62EB4CCFCA690BE1DC3936ED08A7B8F14FE0CFF9";;
+  '16.04') SHA="a02649b1bada2a05fe40261af0311aa4cef01372ce37d8d158b5f0b598ef1138";;
+  '18.04') SHA="79f4dd72bc66ed779da22c0a561de0b0cf341201fc970930d9da9144c2824559";;
+  '20.04') SHA="4d0ee19617ebd696527e966a35a00f94ca3e83c850efdde10cd4b27369a17288";;
         *) bailmsg "Ubuntu ${OSV} not supported!"
 esac
 
