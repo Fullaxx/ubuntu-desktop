75bea7f938c7f42005d3f36b477be32307400e8c
Brett Kuskie <fullaxx@gmail.com>
2022-04-08 13:52:05 -0600

    Add tests/deadbeef.sh

---
+++ tests/deadbeef.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+export DEBIAN_FRONTEND="noninteractive"
+DBVERS="1.8.8"
+URL="https://downloads.sourceforge.net/project/deadbeef/travis/linux/${DBVERS}/deadbeef-static_${DBVERS}-1_x86_64.tar.bz2"
+TARBALL="/tmp/deadbeef.tar.bz2"
+
+wget ${URL} -O ${TARBALL}
