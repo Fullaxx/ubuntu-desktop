658b88a746466966d20d7e76e0de93abaabf0a4d
Brett Kuskie <fullaxx@gmail.com>
2022-09-18 07:53:44 -0600

    Update scripts/firefox.sh

--- scripts/firefox.sh
+++ scripts/firefox.sh
@@ -2,12 +2,11 @@
 
 BINARY="firefox"
 
-OSV=`grep VERSION_ID /etc/os-release | cut -d\" -f2`
-
 BINARYLOC=`which ${BINARY}`
 if [ "$?" -ne "0" ]; then
   set -e
 
+  OSV=`grep VERSION_ID /etc/os-release | cut -d\" -f2`
   case "${OSV}" in
     '22.04') xterm -T InstallFirefox -g 100x30 -e sudo /app/scripts/install_firefox_repo.sh ;;
           *) xterm -T InstallFirefox -g 100x30 -e sudo /app/scripts/install_firefox.sh ;;
