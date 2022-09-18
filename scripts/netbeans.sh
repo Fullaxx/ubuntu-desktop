b92705ea21ef2f4bc9ebd1b5b3bf00adde967bc5
Brett Kuskie <fullaxx@gmail.com>
2022-09-18 07:53:14 -0600

    Update netbeans to error with Ubuntu 22.04

--- scripts/netbeans.sh
+++ scripts/netbeans.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+bailmsg()
+{
+  xmessage "$1" &
+  exit 1
+}
+
 export DEBIAN_FRONTEND="noninteractive"
 BINARY="netbeans"
 
@@ -7,8 +13,15 @@ BINARYLOC=`which ${BINARY}`
 if [ "$?" -ne "0" ]; then
   set -e
 
-  xterm -T AptGetUpdate -g 100x30 -e sudo apt-get update
-  xterm -T AptGetInstall -g 100x30 -e sudo apt-get install -y ${BINARY}
+  OSV=`grep VERSION_ID /etc/os-release | cut -d\" -f2`
+  case "${OSV}" in
+    '18.04') SOURCE="ubuntu" ;;
+    '20.04') SOURCE="ubuntu" ;;
+          *) bailmsg "Ubuntu ${OSV} not supported!"
+  esac
+
+  apt-get update
+  apt-get install -y ${BINARY}
 fi
 
 ${BINARY}
