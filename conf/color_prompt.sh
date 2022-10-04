8a25efb347ab37a7b2e977dfb9f7f9dcd9ca7f27
Brett Kuskie <fullaxx@gmail.com>
2022-10-04 05:39:45 -0600

    Add bash configuration files

---
+++ conf/color_prompt.sh
@@ -0,0 +1,2 @@
+source /opt/bash/bash.colors
+PS1="\[${BIWhite}\][\[${BIBlue}\]\t \[${BIGreen}\]\u\[${BIWhite}\]@\[${BIRed}\]\h \[${BIWhite}\](\[${BICyan}\]\w\[${BIWhite}\])\[${BIWhite}\]]\$ \[${Color_Off}\]"
