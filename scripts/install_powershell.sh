7823f0c9af81254cd938cd9c13afa37d08dd5881
Brett Kuskie <fullaxx@gmail.com>
2022-11-13 03:09:15 -0700

    Update to powershell-lts_7.2.7

--- scripts/install_powershell.sh
+++ scripts/install_powershell.sh
@@ -9,8 +9,8 @@ bailmsg()
 }
 
 export DEBIAN_FRONTEND="noninteractive"
-URL="https://github.com/PowerShell/PowerShell/releases/download/v7.2.2/powershell-lts_7.2.2-1.deb_amd64.deb"
-SHA="BF822A5033403C396126818985FEE79AF163B62BB015603D81400E90AC9E32C7"
+URL="https://github.com/PowerShell/PowerShell/releases/download/v7.2.7/powershell-lts_7.2.7-1.deb_amd64.deb"
+SHA="DC85567C9A52E16EBED727F41389DE0B8E3275437E4B7A3905BC894F359A24F9"
 DEB="/tmp/powershell.deb"
 
 wget ${URL} -O ${DEB}
