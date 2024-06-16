8b300e36cb6f77a74c982c7dab4fda3e78b847e4
Brett Kuskie <fullaxx@gmail.com>
2024-06-16 05:40:14 -0600

    Update wallpapers

--- bin/set_wallpaper.sh
+++ bin/set_wallpaper.sh
@@ -6,15 +6,15 @@ if [ -z "$1" ]; then
 fi
 
 case "$1" in
-  1) WPURL="https://rapidlinux.org/udwp/metallurgy1HDfree.jpg" ;;
-  2) WPURL="https://rapidlinux.org/udwp/portals1HDfree.jpg" ;;
-  3) WPURL="https://rapidlinux.org/udwp/cronusrising2k141HDfree.jpg" ;;
-  4) WPURL="https://rapidlinux.org/udwp/harbinger2k91HDfree.jpg" ;;
-  5) WPURL="https://rapidlinux.org/udwp/arcana2HDfree.jpg" ;;
-  6) WPURL="https://rapidlinux.org/udwp/rivenHDfree.jpg" ;;
-  7) WPURL="https://rapidlinux.org/udwp/circularlogicHDfree.jpg" ;;
-  8) WPURL="https://rapidlinux.org/udwp/gardener2k121HDfree.jpg" ;;
-  9) WPURL="https://rapidlinux.org/udwp/overseer2k141_single_1920x1080.jpg" ;;
+  1) WPURL="https://www.rapidlinux.org/udwp/metallurgy1HDfree.jpg" ;;
+  2) WPURL="https://www.rapidlinux.org/udwp/portals1HDfree.jpg" ;;
+  3) WPURL="https://www.rapidlinux.org/udwp/cronusrising2k141HDfree.jpg" ;;
+  4) WPURL="https://www.rapidlinux.org/udwp/harbinger2k91HDfree.jpg" ;;
+  5) WPURL="https://www.rapidlinux.org/udwp/arcana2HDfree.jpg" ;;
+  6) WPURL="https://www.rapidlinux.org/udwp/rivenHDfree.jpg" ;;
+  7) WPURL="https://www.rapidlinux.org/udwp/circularlogicHDfree.jpg" ;;
+  8) WPURL="https://www.rapidlinux.org/udwp/gardener2k121HDfree.jpg" ;;
+  9) WPURL="https://www.rapidlinux.org/udwp/overseer2k141_single_1920x1080.jpg" ;;
   *) WPURL="0" ;;
 esac
 
