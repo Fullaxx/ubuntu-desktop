0843c3db4628ad6730978cb446b4a541a3cff93c
corahereraecn62 <corahereraecn62@gmail.com>
2025-02-22 13:52:24 +0530

    Update qbittorrent.sh

--- scripts/qbittorrent.sh
+++ scripts/qbittorrent.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
 
-export DEBIAN_FRONTEND="noninteractive"
-BINARY="qbittorrent"
-
-BINARYLOC=`which ${BINARY}`
-if [ "$?" -ne "0" ]; then
-  set -e
-
-  xterm -T AptGetUpdate -g 100x30 -e sudo apt-get update
-  xterm -T AptGetInstall -g 100x30 -e sudo apt-get install -y ${BINARY}
-fi
-
-${BINARY}
